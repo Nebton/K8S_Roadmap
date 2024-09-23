8e53fc6335bd3c09c913e51b8f241d143ffb3998
Nebton <21977392+Nebton@users.noreply.github.com>
2024-09-23 23:21:24 +0200

    Create monitoring ns by default

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -12,6 +12,7 @@ resource "helm_release" "prometheus" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
   namespace  = var.environment
+  create_namespace = true
 
   values = [
     file("${var.config_path}/prometheus-values.yaml")
