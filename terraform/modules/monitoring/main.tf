01f88206b84b49852093185236cafb36c621bf6e
Nebton <21977392+Nebton@users.noreply.github.com>
2024-09-24 00:52:26 +0200

    Added more prometheus rules

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -7,17 +7,22 @@ terraform {
   }
 }
 
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = var.environment
+  }
+}
+
 resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-  namespace  = var.environment
-  replace = true
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
   values = [
     file("${var.config_path}/prometheus-values.yaml")
   ]
-
-set {
+  
+  set {
     name  = "additionalServiceMonitors[0].namespace"
     value = var.environment
   }
@@ -26,12 +31,10 @@ set {
     name  = "additionalServiceMonitors[1].namespace"
     value = var.environment
   }
+
+  replace = true
+  force_update  = true
+  cleanup_on_fail = true
+
+  depends_on = [kubernetes_namespace.monitoring]
 }
-
-resource "kubernetes_namespace" "app_namespace" {
-  metadata {
-    name = var.environment
-  }
-}
-
-
