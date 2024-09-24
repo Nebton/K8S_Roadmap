a7c824b52579f54d642e9be9863ed801bf95150d
Nebton <21977392+Nebton@users.noreply.github.com>
2024-09-24 02:15:28 +0200

    Roll back to working monitoring stack

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -7,22 +7,17 @@ terraform {
   }
 }
 
-resource "kubernetes_namespace" "monitoring" {
-  metadata {
-    name = var.environment
-  }
-}
-
 resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+  namespace  = var.environment
+
   values = [
     file("${var.config_path}/prometheus-values.yaml")
   ]
-  
-  set {
+
+set {
     name  = "additionalServiceMonitors[0].namespace"
     value = var.environment
   }
@@ -31,10 +26,20 @@ resource "helm_release" "prometheus" {
     name  = "additionalServiceMonitors[1].namespace"
     value = var.environment
   }
+}
 
-  replace = true
-  force_update  = true
-  cleanup_on_fail = true
 
-  depends_on = [kubernetes_namespace.monitoring]
+resource "kubectl_manifest" "node_exporter_deployment" {
+  yaml_body  =  templatefile( "${var.config_path}/node-exporter-deployment.yaml", {namespace = var.environment})
+  depends_on = [helm_release.prometheus]
+}
+
+resource "kubectl_manifest" "node_exporter_service" {
+  yaml_body  =  templatefile( "${var.config_path}/node-exporter-service.yaml", {namespace = var.environment})
+  depends_on = [helm_release.prometheus, kubectl_manifest.node_exporter_deployment]
+}
+
+resource "kubectl_manifest" "flask-app-monitor" {
+  yaml_body =  templatefile( "${var.config_path}/flask-app-monitor.yaml", {namespace = var.environment})
+  depends_on = [helm_release.prometheus]
 }
