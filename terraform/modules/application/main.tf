42d98f7d3d0df84ce6c66921cb60c4c140dafc14
Nebton <21977392+Nebton@users.noreply.github.com>
2024-09-26 05:01:37 +0200

    Fixed up some more istio stuff

--- terraform/modules/application/main.tf
+++ terraform/modules/application/main.tf
@@ -1,7 +1,16 @@
+resource "kubernetes_namespace" "app_namespace" {
+  metadata {
+    name = var.environment
+    labels = {
+      istio-injection = "enabled"
+    }
+  }
+}
+
 resource "helm_release" "k8s_roadmap" {
   name       = "k8s-roadmap"
   chart      = var.helm_chart_path
-  namespace  = var.environment
+  namespace  = kubernetes_namespace.app_namespace.metadata[0].name 
   
   values = [
     file("${var.helm_chart_path}/values.yaml")
--- terraform/modules/istio/main.tf
+++ terraform/modules/istio/main.tf
@@ -1,3 +1,10 @@
+# Application Namespace
+resource "kubernetes_namespace" "app_namespace" {
+  metadata {
+    name = var.environment 
+  }
+}
+
 # Istio Base
 resource "helm_release" "istio_base" {
   name             = "istio-base"
@@ -5,13 +12,15 @@ resource "helm_release" "istio_base" {
   chart            = "base"
   version          = "1.23.2"  # Pin the version
   namespace        = var.environment
-  create_namespace = true
-  timeout          = 900  # 15 minutes
+  timeout          = 300 
+
+  depends_on = [kubernetes_namespace.app_namespace]
 
   set {
     name  = "global.istioNamespace"
     value = var.environment 
   }
+
 }
 
 # Istiod
@@ -19,9 +28,9 @@ resource "helm_release" "istiod" {
   name       = "istiod"
   repository = "https://istio-release.storage.googleapis.com/charts"
   chart      = "istiod"
-  version    = "1.23.2"  # Pin the version
+  version    = "1.23.2"  
   namespace  = var.environment
-  timeout    = 900  # 15 minutes
+  timeout    = 900  
 
   depends_on = [helm_release.istio_base]
 
@@ -55,14 +64,3 @@ resource "helm_release" "istio_ingress" {
 
 }
 
-# Application Namespace
-resource "kubernetes_namespace" "app_namespace" {
-  metadata {
-    name = var.environment 
-    labels = {
-      "istio-injection" = "enabled"
-    }
-  }
-
-  depends_on = [helm_release.istiod]
-}
