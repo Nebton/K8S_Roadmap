9a9e17fc4d05ee0601b15e5aadb237a6580a5f91
Nebton <21977392+Nebton@users.noreply.github.com>
2024-09-27 05:33:06 +0200

    Fix backend round robin destination rule name

--- terraform/modules/istio/main.tf
+++ terraform/modules/istio/main.tf
@@ -118,7 +118,7 @@ resource "kubectl_manifest" "istio_ingress_servicemonitor" {
   depends_on = [kubectl_manifest.istio_ingress_gateway]
 }
 
-resource "kubectl_manifest" "frontend_backend_route" {
+resource "kubectl_manifest" "backend_round_robin" {
   yaml_body  =  templatefile( "${var.config_path}/backend-destination.yaml", {})
   depends_on = [kubectl_manifest.istio_ingress_gateway]
   override_namespace = var.injected_namespace
