b4794938900ad50855666c5648b20a9c22a79506
Nebton <21977392+Nebton@users.noreply.github.com>
2024-10-13 05:03:24 +0200

    debugging still..

--- terraform/modules/vault/main.tf
+++ terraform/modules/vault/main.tf
@@ -72,57 +72,57 @@ output "debug_vault_init_result" {
   sensitive = true
 }
 
-resource "null_resource" "vault_unseal" {
-  depends_on = [data.external.vault_init]
-
-  provisioner "local-exec" {
-    command = <<-EOT
-      kubectl exec -n ${var.environment} vault-0 -- vault operator unseal ${data.external.vault_init.result.unseal_keys_b64[0]} &&
-      kubectl exec -n ${var.environment} vault-0 -- vault operator unseal ${data.external.vault_init.result.unseal_keys_b64[1]} &&
-      kubectl exec -n ${var.environment} vault-0 -- vault operator unseal ${data.external.vault_init.result.unseal_keys_b64[2]}
-    EOT
-  }
-}
-
-provider "vault" {
-  address = "http://127.0.0.1:8200"  
-  token   = data.external.vault_init.result.root_token
-}
-
-resource "vault_mount" "kv" {
-  depends_on = [null_resource.vault_unseal]
-  path       = "secret"
-  type       = "kv"
-  options    = { version = "2" }
-}
-
-resource "vault_mount" "db" {
-  depends_on = [null_resource.vault_unseal]
-  path       = "database"
-  type       = "database"
-}# # Configure PostgreSQL connection
-# resource "vault_database_secret_backend_connection" "postgres" {
-#   backend       = vault_mount.db.path
-#   name          = "postgres"
-#   allowed_roles = ["flask-app-role"]
+# resource "null_resource" "vault_unseal" {
+#   depends_on = [data.external.vault_init]
 #
-#   postgresql {
-#     connection_url = "postgresql://{{username}}:{{password}}@postgresql.${kubernetes_namespace.postgresql.metadata[0].name}.svc.cluster.local:5432/flaskdb?sslmode=disable"
+#   provisioner "local-exec" {
+#     command = <<-EOT
+#       kubectl exec -n ${var.environment} vault-0 -- vault operator unseal ${data.external.vault_init.result.unseal_keys_b64[0]} &&
+#       kubectl exec -n ${var.environment} vault-0 -- vault operator unseal ${data.external.vault_init.result.unseal_keys_b64[1]} &&
+#       kubectl exec -n ${var.environment} vault-0 -- vault operator unseal ${data.external.vault_init.result.unseal_keys_b64[2]}
+#     EOT
 #   }
 # }
 #
-# # Create a role for the database secrets engine
-# resource "vault_database_secret_backend_role" "flask_app_role" {
-#   backend     = vault_mount.db.path
-#   name        = "flask-app-role"
-#   db_name     = vault_database_secret_backend_connection.postgres.name
-#   default_ttl = "1h"
-#   max_ttl     = "24h"
-#
-#   creation_statements = [
-#     "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';",
-#     "GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";"
-#   ]
+# provider "vault" {
+#   address = "http://127.0.0.1:8200"  
+#   token   = data.external.vault_init.result.root_token
 # }
 #
+# resource "vault_mount" "kv" {
+#   depends_on = [null_resource.vault_unseal]
+#   path       = "secret"
+#   type       = "kv"
+#   options    = { version = "2" }
+# }
 #
+# resource "vault_mount" "db" {
+#   depends_on = [null_resource.vault_unseal]
+#   path       = "database"
+#   type       = "database"
+# }# # Configure PostgreSQL connection
+# # resource "vault_database_secret_backend_connection" "postgres" {
+# #   backend       = vault_mount.db.path
+# #   name          = "postgres"
+# #   allowed_roles = ["flask-app-role"]
+# #
+# #   postgresql {
+# #     connection_url = "postgresql://{{username}}:{{password}}@postgresql.${kubernetes_namespace.postgresql.metadata[0].name}.svc.cluster.local:5432/flaskdb?sslmode=disable"
+# #   }
+# # }
+# #
+# # # Create a role for the database secrets engine
+# # resource "vault_database_secret_backend_role" "flask_app_role" {
+# #   backend     = vault_mount.db.path
+# #   name        = "flask-app-role"
+# #   db_name     = vault_database_secret_backend_connection.postgres.name
+# #   default_ttl = "1h"
+# #   max_ttl     = "24h"
+# #
+# #   creation_statements = [
+# #     "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';",
+# #     "GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";"
+# #   ]
+# # }
+# #
+# #
